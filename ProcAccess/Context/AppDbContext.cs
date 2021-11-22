cf0d7c8785953c451b1ef2bc4a10bb51b6533950
Camila <fcamilalima@gmail.com>
2021-11-22 12:56:37 -0300

    Adicionar arquivos de projeto.

---
+++ ProcAccess/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProcAccess.Models;
+
+namespace ProcAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+        public DbSet<Processo> Processos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }    
+
+}
+}
---
+++ ProcAccess/Models/Processo.cs
@@ -0,0 +1,12 @@
+﻿namespace ProcAccess.Models
+{
+    public class Processo
+    {
+        public int ProcessoId { get; set; }
+        public int Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
